329dd8a95669765671814005ca8a1938190f80cb
Omar Moreno Duran <68525419+moreno-omar@users.noreply.github.com>
2025-12-16 13:56:37 -0800

    Add files via upload
    minimal config for nvim

---
+++ nvim_init.lua
@@ -0,0 +1,14 @@
+-- BASIC OPTIONS
+vim.o.number = true
+vim.o.relativenumber = false
+vim.o.termguicolors = true
+
+-- key timeout (built-in which-key-like behavior)
+vim.o.timeout = true
+vim.o.timeoutlen = 500
+
+-- DARK THEME (tokyonight)
+vim.cmd("colorscheme tokyonight_lite")
+
+-- standarize tab
+vim.o.tabstop = 4
